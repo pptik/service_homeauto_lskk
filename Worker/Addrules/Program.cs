ef230f146094f0c0f3462b4e25a94da16e2b9b61
nurmanhariyanto <nurman.hariyanto13@gmail.com>
2020-06-19 17:06:23 +0700

    worker registration and rules

---
+++ Worker/Addrules/Program.cs
@@ -0,0 +1,90 @@
+﻿using System.Diagnostics;
+using System.Text;
+using System;
+using System.IO;
+using Microsoft.Data.Sqlite;
+
+namespace Addrules
+{
+    class Program
+    {
+        public static string btnName = "";
+        public static string vlButton = "";
+        public static string lmpName = "";
+        public static string vLamp = "";
+        static void Main(string[] args)
+        {
+            databaseInsert();
+
+
+
+
+        }
+
+
+
+
+        public static void databaseInsert()
+        {
+
+            var connectionStringBuilder = new SqliteConnectionStringBuilder();
+            connectionStringBuilder.DataSource = "/home/nurman/Documents/Code/Worker/Rulesiot.db";
+            using (var connectionDB = new SqliteConnection(connectionStringBuilder.ConnectionString))
+            {
+
+
+                connectionDB.Open();
+
+                // var delTableCmd = connectionDB.CreateCommand();
+                // delTableCmd.CommandText = "DROP TABLE IF EXISTS Rules";
+                // delTableCmd.ExecuteNonQuery();
+
+                // var createTableCmd = connectionDB.CreateCommand();
+                // createTableCmd.CommandText = "CREATE TABLE Rules(Rulesid INTEGER PRIMARY KEY AUTOINCREMENT,Guidsensor VARCHAR(255) NOT NULL,Valuesensor VARCHAR(255) NOT NULL,Guidaktuator VARCHAR(255) NOT NULL,Valueaktuator VARCHAR(255) NOT NULL)";
+                // createTableCmd.ExecuteNonQuery();
+
+                using (var transaction = connectionDB.BeginTransaction())
+                {
+                    var insertCmd = connectionDB.CreateCommand();
+
+                    Console.WriteLine("Enter Name Button:");
+                    btnName = Console.ReadLine();
+                    Console.WriteLine("Your Button Is: " + btnName);
+
+                    Console.WriteLine("Enter Value Button:");
+                    vlButton = Console.ReadLine();
+                    Console.WriteLine("Your Button Is: " + vlButton);
+
+                    Console.WriteLine("Enter Name Lamp:");
+                    lmpName = Console.ReadLine();
+                    Console.WriteLine("Your Button Is: " + lmpName);
+
+                    Console.WriteLine("Enter Name Button:");
+                    vLamp = Console.ReadLine();
+                    Console.WriteLine("Your Button Is: " + vLamp);
+
+                    insertCmd.CommandText = "INSERT INTO Rules (Guidsensor,Valuesensor,Guidaktuator,Valueaktuator) Values(@Guidsensor,@Valuesensor,@Guidaktuator,@Valueaktuator)";
+                    insertCmd.Parameters.AddWithValue("@Guidsensor", btnName);
+                    insertCmd.Parameters.AddWithValue("@Valuesensor", vlButton);
+                    insertCmd.Parameters.AddWithValue("@Guidaktuator", lmpName);
+                    insertCmd.Parameters.AddWithValue("@Valueaktuator", vLamp);
+                    Console.WriteLine(btnName);
+                    Console.WriteLine(vlButton);
+                    Console.WriteLine(lmpName);
+                    Console.WriteLine(vLamp);
+
+                    insertCmd.ExecuteNonQuery();
+
+                    transaction.Commit();
+                    Console.WriteLine("Berhasil update data");
+
+
+
+
+                }
+
+
+            }
+        }
+    }
+}
---
+++ Worker/RmqServices/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using Microsoft.Data.Sqlite;
+
+
+
+
+namespace RmqServices
+{
+    class Program
+    {
+        public static string inputGuid = "";
+        public static string valueInput = "";
+        public static string outputGuid = "";
+        public static string valueOutput = "";
+        public static string messageSend = "";
+        public static string hostname = "167.205.7.226";
+        public static int port = 5672;
+        public static string user = "homeauto";
+        public static string pass = "homeauto12345!";
+        public static string vhost = "/homeauto";
+        public static string exchange = "amq.topic";
+        public static string queue = "Homeauto";
+        public static string routingKeySubscribe = "HomeautoIn";
+        public static string routingKeyPublish = "HomeautoOut";
+
+        public static string pathDatabase = "/home/nurman/Documents/Code/Worker/homeauto.db";
+
+
+
+        static void Main(string[] args)
+        {
+
+            var connectionStringBuilder = new SqliteConnectionStringBuilder();
+            connectionStringBuilder.DataSource = pathDatabase;
+            var connectionDB = new SqliteConnection(connectionStringBuilder.ConnectionString);
+
+
+            var factory = new ConnectionFactory() { HostName = hostname, Port = port, UserName = user, Password = pass, VirtualHost = vhost };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+
+                    //Console.Write("[x] Received {0}",message);
+                    string[] dataParsing = message.Split('#');
+                    foreach (var datas in dataParsing)
+                    {
+                        //System.Console.WriteLine($"{datas}>");
+                        inputGuid = dataParsing[0];
+                        valueInput = dataParsing[1];
+
+                    }
+
+                    connectionDB.Open();
+                    //var transaction = connectionDB.BeginTransaction();
+                    // var insertCmd = connectionDB.CreateCommand();
+                    // insertCmd.CommandText = "INSERT INTO Rules (Guidsensor,Valuesensor,Guidaktuator,Valueaktuator) Values(@Guidsensor,@Valuesensor,@Guidaktuator,@Valueaktuator)";
+                    // insertCmd.Parameters.AddWithValue("@Guidsensor", inputGuid);
+                    // insertCmd.Parameters.AddWithValue("@Valuesensor", valueInput);
+                    // insertCmd.Parameters.AddWithValue("@Guidaktuator", outputGuid);
+                    // insertCmd.Parameters.AddWithValue("@Valueaktuator", valueOutput);
+
+
+                    // insertCmd.ExecuteNonQuery();
+
+
+                    var selectCmd = connectionDB.CreateCommand();
+                    selectCmd.CommandText = "SELECT * FROM activityiot  WHERE input_guid=@Guidinput AND input_value=@Valueinput ";
+
+                    selectCmd.Parameters.AddWithValue("@Guidinput", inputGuid);
+                    selectCmd.Parameters.AddWithValue("@Valueinput", valueInput);
+
+                    using (var reader = selectCmd.ExecuteReader())
+                    {
+
+                        while (reader.Read())
+                        {
+                            outputGuid = reader.GetString(3);
+                            valueOutput = reader.GetString(4);
+
+
+                            messageSend = outputGuid + "#" + valueOutput;
+                            channel.BasicPublish(
+                            exchange: exchange,
+                            routingKey: routingKeyPublish,
+                            basicProperties: null,
+                            body: Encoding.UTF8.GetBytes(messageSend)
+
+                        );
+                        }
+
+
+
+
+                        connectionDB.Close();
+                    }
+
+
+
+                };
+                channel.BasicConsume(queue: queue,
+                                     autoAck: true,
+                                     consumer: consumer
+                                    );
+                Console.WriteLine("Press Any Key to Close");
+                Console.ReadLine();
+            }
+
+        }
+
+
+
+
+
+
+
+    }
+}
