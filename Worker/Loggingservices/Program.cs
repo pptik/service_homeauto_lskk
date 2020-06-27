712e82c216dc036e58a2298ecd8bb664b1651e6d
nurmanhariyanto <nurman.hariyanto13@gmail.com>
2020-06-27 22:41:00 +0700

    get time

---
+++ Worker/Loggingservices/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace Loggingservices
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime localDate = DateTime.Now;
+            String[] cultureNames = { "en-US", "en-GB", "fr-FR",
+                                "de-DE", "ru-RU" };
+
+            foreach (var cultureName in cultureNames)
+            {
+                var culture = new CultureInfo(cultureName);
+                Console.WriteLine("{0}: {1}", cultureName,
+                                  localDate.ToString(culture));
+            }
+        }
+    }
+}
