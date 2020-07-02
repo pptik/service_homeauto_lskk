25e148cc1e8f0d1906d3b160325b13ca8af154a5
nurmanhariyanto <nurman.hariyanto13@gmail.com>
2020-07-02 14:25:13 +0700

    update service rule

--- Worker/Loggingservices/Program.cs
+++ Worker/Loggingservices/Program.cs
@@ -7,16 +7,14 @@ namespace Loggingservices
     {
         static void Main(string[] args)
         {
-            DateTime localDate = DateTime.Now;
-            String[] cultureNames = { "en-US", "en-GB", "fr-FR",
-                                "de-DE", "ru-RU" };
 
-            foreach (var cultureName in cultureNames)
-            {
-                var culture = new CultureInfo(cultureName);
-                Console.WriteLine("{0}: {1}", cultureName,
-                                  localDate.ToString(culture));
-            }
+            /**
+             * Get time from device
+             * */
+            DateTime now = DateTime.Now;
+            Console.WriteLine(now);
+
+            
         }
     }
 }
---
+++ Worker/RuleServices/Program.cs
@@ -0,0 +1,145 @@
+﻿
+using System.Diagnostics;
+using System.Text;
+using System;
+using System.IO;
+using Microsoft.Data.Sqlite;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using Newtonsoft.Json.Linq;
+
+namespace RuleServices
+{
+    class Program
+    {
+
+        /*
+        Device variable
+        */
+        public static string serialNumberOutput = "";
+        public static string valueOutput = "";
+        public static string serialNumberInput = "";
+        public static string valueInput = "";
+
+
+        /////////////////////////////////////////////////////////
+
+        /*
+        MQTT Variable
+        */
+
+        public static string hostname = "192.168.0.5";
+        public static int port = 5672;
+        public static string user = "homeauto";
+        public static string pass = "homeauto12345!";
+        public static string vhost = "/Homeauto";
+        public static string exchange = "amq.topic";
+        public static string queue = "Reg_rule";
+        public static string routingKeySubscribe = "HomeautoIn";
+        public static string routingKeyPublish = "Statusrule";
+
+        /////////////////////////////////////////////////////////
+
+        /*
+        Path Physical Database file
+        */
+        public static string pathDatabase = "/home/nurman/Documents/GitHub/service_homeauto_lskk/Worker/homeauto.db";
+        /////////////////////////////////////////////////////////
+
+
+
+        /*
+        Variable data and temporary
+        */
+        public static string messageSend = "";
+
+        public static string statusRegistered = "1";
+        public static string statusRegister = "0";
+
+
+
+
+
+
+        static void Main(string[] args)
+        {
+
+            var connectionStringBuilder = new SqliteConnectionStringBuilder();
+            connectionStringBuilder.DataSource = pathDatabase;
+            var connectionDB = new SqliteConnection(connectionStringBuilder.ConnectionString);
+
+
+            var factory = new ConnectionFactory() { HostName = hostname, Port = port, UserName = user, Password = pass, VirtualHost = vhost };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    dynamic dataJson = JObject.Parse(message);
+                    serialNumberOutput = dataJson.serialnumber;
+                    valueOutput = dataJson.mac;
+                    serialNumberInput = dataJson.type;
+                    valueInput = dataJson.qty;
+                    Console.WriteLine(serialNumberOutput);
+                    Console.WriteLine(valueOutput);
+                    Console.WriteLine(serialNumberInput);
+                    Console.WriteLine(valueInput);
+
+                    connectionDB.Open();
+
+                    using (var transaction = connectionDB.BeginTransaction())
+                    {
+                        Console.WriteLine("tidak ada data");
+                        var insertCmd = connectionDB.CreateCommand();
+                        insertCmd.CommandText = "INSERT INTO activityiot (input_guid,input_value,output_guid,output_value) Values(@inputguid,@valueinput,@outputguid,@valueoutput)";
+                        insertCmd.Parameters.AddWithValue("@inputguid", serialNumberOutput);
+                        insertCmd.Parameters.AddWithValue("@valueinput", valueOutput);
+                        insertCmd.Parameters.AddWithValue("@outputguid", serialNumberInput);
+                        insertCmd.Parameters.AddWithValue("@valueinput", valueInput);
+                        insertCmd.ExecuteNonQuery();
+                        transaction.Commit();
+                        Console.WriteLine("Success Insert new data");
+
+                        // messageSend = statusRegister;
+
+                        // channel.BasicPublish(
+                        // exchange: exchange,
+                        // routingKey: routingKeyPublish,
+                        // basicProperties: null,
+                        //  body: Encoding.UTF8.GetBytes(messageSend)
+
+                        // );
+                        // Console.WriteLine("status device " + messageSend + " sent");
+                    }
+
+
+
+
+
+
+
+
+                    connectionDB.Close();
+
+
+
+
+                };
+                channel.BasicConsume(queue: queue,
+                                     autoAck: true,
+                                     consumer: consumer
+                                    );
+                Console.WriteLine("Press Any Key to Close");
+                Console.ReadLine();
+            }
+
+        }
+
+
+
+    }
+}
